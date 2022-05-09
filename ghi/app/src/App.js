b2731fa8531a67cc7e5cd35073fac5025e8674cc
Amanda <apkiehm@gmail.com>
2022-05-09 17:07:24 -0600

    Created front end for Models list

--- ghi/app/src/App.js
+++ ghi/app/src/App.js
@@ -1,14 +1,16 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import MainPage from './MainPage';
 import Nav from './Nav';
+import ModelsList from './ModelsList';
 
-function App() {
+function App(props) {
   return (
     <BrowserRouter>
       <Nav />
       <div className="container">
         <Routes>
-          <Route path="/" element={<MainPage />} />
+          <Route index path="/" element={<MainPage />} />
+            <Route path="models" element={<ModelsList models={props.models}/>}/>
         </Routes>
       </div>
     </BrowserRouter>
