59af1d2d50d987579d81402240af8e04c7244b23
cisebaba <cisebabatasi@gmail.com>
2022-05-09 16:46:34 -0700

    inventory frontend progress

--- ghi/app/src/index.js
+++ ghi/app/src/index.js
@@ -8,3 +8,25 @@ root.render(
     <App />
   </React.StrictMode>
 );
+async function loadInventory() {
+  const manufacturersResponse = await fetch('http://localhost:8100/api/manufacturers/');
+  const vehicleModelsResponse = await fetch('http://localhost:8100/api/models/');
+  const automobilesResponse = await fetch('http://localhost:8100/api/automobiles/');
+  if (manufacturersResponse.ok && vehicleModelsResponse.ok && automobilesResponse.ok) {
+    const manufacturersData = await manufacturersResponse.json();
+    const vehicleModelsData = await vehicleModelsResponse.json();
+    const automobilesData = await automobilesResponse.json();
+    root.render(
+      <React.StrictMode>
+        <App manufacturers={manufacturersData.manufacturers} 
+          models={vehicleModelsData.models}
+          automobiles={automobilesData.autos}/>
+      </React.StrictMode>
+    );
+  } else {
+    console.error("manufacturersResponse", manufacturersResponse);
+    console.error("vehicleModelsResponse", vehicleModelsResponse);
+    console.error("automobilesResponse", automobilesResponse);
+  }
+}
+loadInventory();
