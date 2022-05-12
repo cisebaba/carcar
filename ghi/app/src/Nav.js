8e1df6483f551be644c0854c1ae7a23913495aa6
cisebaba <cisebabatasi@gmail.com>
2022-05-12 11:23:28 -0700

    fixed the index

--- ghi/app/src/Nav.js
+++ ghi/app/src/Nav.js
@@ -11,35 +11,35 @@ function Nav() {
         <div className="collapse navbar-collapse" id="navbarSupportedContent">
           <ul className="navbar-nav me-auto mb-2 mb-lg-0">
             <li className="nav-item dropdown">
-              <a class="nav-link dropdown-toggle" data-bs-toggle="dropdown" href="#" role="button" aria-expanded="false">Manufacturer</a>
+              <a className="nav-link dropdown-toggle" data-bs-toggle="dropdown" href="#" role="button" aria-expanded="false">Manufacturer</a>
               <ul className="dropdown-menu">
                 <li><NavLink className="dropdown-item" to="manufacturers" role="button">Manufacturer List</NavLink></li>
                 <li><NavLink className="dropdown-item" to="manufacturers/new" role="button">Add a Manufacturer</NavLink></li>
               </ul>
             </li>
             <li className="nav-item dropdown">
-              <a class="nav-link dropdown-toggle" data-bs-toggle="dropdown" href="#" role="button" aria-expanded="false">Vehicle</a>
+              <a className="nav-link dropdown-toggle" data-bs-toggle="dropdown" href="#" role="button" aria-expanded="false">Vehicle</a>
               <ul className="dropdown-menu">
                 <li><NavLink className="dropdown-item" to="/models" role="button">Vehicle Models List</NavLink></li>
                 <li><NavLink className="dropdown-item" to="/models/new" role="button">Add a Vehicle Model</NavLink></li>
               </ul>
             </li>
             <li className="nav-item dropdown">
-              <a class="nav-link dropdown-toggle" data-bs-toggle="dropdown" href="#" role="button" aria-expanded="false">Autos</a>
+              <a className="nav-link dropdown-toggle" data-bs-toggle="dropdown" href="#" role="button" aria-expanded="false">Autos</a>
               <ul className="dropdown-menu">
                 <li><NavLink className="dropdown-item" to="automobiles" role="button">Automobiles List</NavLink></li>
                 <li><NavLink className="dropdown-item" to="/automobiles/new" role="button">Add an Auto</NavLink></li>
               </ul>
             </li>
             <li className="nav-item dropdown">
-              <a class="nav-link dropdown-toggle" data-bs-toggle="dropdown" href="#" role="button" aria-expanded="false">Sales</a>
+              <a className="nav-link dropdown-toggle" data-bs-toggle="dropdown" href="#" role="button" aria-expanded="false">Sales</a>
               <ul className="dropdown-menu">
                 <li><NavLink className="dropdown-item" to="/salespeople" role="button">Add a Sales Person</NavLink></li>
                 <li><NavLink className="dropdown-item" to="/customers" role="button">Add a potential customer</NavLink></li>
               </ul>
             </li>
             <li className="nav-item dropdown">
-              <a class="nav-link dropdown-toggle" data-bs-toggle="dropdown" href="#" role="button" aria-expanded="false">Service</a>
+              <a className="nav-link dropdown-toggle" data-bs-toggle="dropdown" href="#" role="button" aria-expanded="false">Service</a>
               <ul className="dropdown-menu">
                 <li><NavLink className="dropdown-item" to="technicians/new" role="button">Add a Technician</NavLink></li>
                 <li><NavLink className="dropdown-item" to="appointments/new" role="button">Add an Appointment</NavLink></li>
